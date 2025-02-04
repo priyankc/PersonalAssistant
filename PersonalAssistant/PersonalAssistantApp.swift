8ef96676741da187830b6dacfac044397541e45f
Priyank Chodisetti <priyank.ch@gmail.com>
2025-02-03 23:19:19 -0800

    Initial Commit

---
+++ PersonalAssistant/PersonalAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAssistantApp.swift
+//  PersonalAssistant
+//
+//  Created by Priyank Chodisetti on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonalAssistantTests/PersonalAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAssistantTests.swift
+//  PersonalAssistantTests
+//
+//  Created by Priyank Chodisetti on 2/3/25.
+//
+
+import Testing
+@testable import PersonalAssistant
+
+struct PersonalAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
